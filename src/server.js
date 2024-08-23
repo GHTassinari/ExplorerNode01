360f2ece08ae94d81fbf240470a12eb1c17af542
guilherme <guilhermehen.tassinari@gmail.com>
2024-08-23 10:40:19 -0300

    Testing query parameters

--- src/server.js
+++ src/server.js
@@ -2,12 +2,20 @@ const express = require ("express");
 
 const app = express();
 
+/* While using Route parameters, providing the values are obligatory, ir order to access the GET */
 app.get("/message/:id/:user", (req, res) => {
   const { id , user } = req.params;
 
 
   res.send(`Id da mensagem: ${id}.
     Para o usuário: ${user}`)
+})
+
+/* While using query parameters, parameters are optional, and you can access the get URL without providing one */
+app.get("/users", (req, res) => {
+  const { page, limit } = req.query;
+
+  res.send(`Página: ${page}. Mostrar: ${limit}`);
 })
 
 const PORT = 3333;

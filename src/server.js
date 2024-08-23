2c3c86559c7b59022edd61f58c8ab72179942b58
guilherme <guilhermehen.tassinari@gmail.com>
2024-08-23 12:04:39 -0300

    creating routes files

--- src/server.js
+++ src/server.js
@@ -1,14 +1,11 @@
-const express = require ("express");
+const express = require("express");
+
+const routes = require("./routes"); /* If there isn't any name specifying the file, it will automatically open index.js */
 
 const app = express();
 app.use(express.json());
 
-app.post("/users", (req, res) => {
-  const { name, email, password } = req.body;
-
-  res.json({ name, email, password });
-})
-
+app.use(routes);
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));

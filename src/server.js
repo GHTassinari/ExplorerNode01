280aac91b89e71071979d9db090d5a5a7b9076cb
guilherme <guilhermehen.tassinari@gmail.com>
2024-08-23 10:49:50 -0300

    Starting to learn the post method

--- src/server.js
+++ src/server.js
@@ -2,21 +2,11 @@ const express = require ("express");
 
 const app = express();
 
-/* While using Route parameters, providing the values are obligatory, ir order to access the GET */
-app.get("/message/:id/:user", (req, res) => {
-  const { id , user } = req.params;
+app.post("/users", (req, res) => {
 
-
-  res.send(`Id da mensagem: ${id}.
-    Para o usuário: ${user}`)
+  res.send("Você chamou o post");
 })
 
-/* While using query parameters, parameters are optional, and you can access the get URL without providing one */
-app.get("/users", (req, res) => {
-  const { page, limit } = req.query;
-
-  res.send(`Página: ${page}. Mostrar: ${limit}`);
-})
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));

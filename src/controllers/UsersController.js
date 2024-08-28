1f0d53441a4a697690bb36528aa050c1a2335ac6
guilherme <guilhermehen.tassinari@gmail.com>
2024-08-28 11:31:56 -0300

    Adding controllers to the project

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,11 @@
+class UsersController {
+
+  create(req, res) {
+    const { name, email, password } = req.body;
+
+    res.json({ name, email, password });
+  }
+  
+}
+
+module.exports = UsersController;

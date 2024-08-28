e083323061f3f2c7eda5fbf4333bb9aa2b46e002
guilherme <guilhermehen.tassinari@gmail.com>
2024-08-28 12:03:37 -0300

    Adding exceptions to the project, for internal server erros and when the name wasn't sent in the request body

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,8 +1,13 @@
+const AppError = require ("../utils/AppError");
+
 class UsersController {
 
   create(req, res) {
     const { name, email, password } = req.body;
-
+    
+    if(!name){
+      throw new AppError("The name is obligatory to create a new user!")
+    }
     res.status(201).json({ name, email, password });
   }
 
